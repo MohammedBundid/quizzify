a7204b7ad04c76af4974ce4805665d3e4ead5c44
mohammed <mohabdullahi2001@gmail.com>
2023-07-07 18:12:42 +0300

    initial commit

---
+++ js/loader.js
@@ -0,0 +1,9 @@
+// Wait for the page to load
+$(document).ready(function() {
+    // Simulate a loading delay (e.g., 3 seconds)
+    setTimeout(function() {
+      // Redirect to the desired page
+      window.location.href = "/pages/science.html";
+    }, 3000);
+  });
+  
---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $('.card').click(function () { 
+    if(this.id == 'scienceCard'){
+        location.href = 'loader.html';
+    }
+    
+  });
+    
+});
